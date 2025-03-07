946ba90e80238dcbffd5fd76ac0d942e7e0164ab
mong-sathowai-marma <mongsathowai7@gmail.com>
2025-03-07 21:09:22 +0600

    Add files via upload

---
+++ create a new string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string S,T;
+    int a=0,b=0;
+    cin>>S>>T;
+    a=S.size();
+    b=T.size();
+    cout<<a<<" "<<b<<endl;
+    cout<<S<<" "<<T<<endl;
+    return 0;
+}
+
